168b3030447a5d8a2eb56ea3ca2ea57067022099
Stephen Bell <stephen.l.bell@gmail.com>
2021-03-26 14:56:35 -0400

    Initialize new project

---
+++ src/test/java/ffb/quizsite/quizEngine/QuizEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package ffb.quizsite.quizEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
