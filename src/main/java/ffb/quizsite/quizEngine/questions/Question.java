c97887b1cd59f1bf206f23de477ad30ce80bf3c9
Stephen Bell <stephen.l.bell@gmail.com>
2021-03-26 15:10:17 -0400

    Add constructors and getters/setters

--- src/main/java/ffb/quizsite/quizEngine/questions/Question.java
+++ src/main/java/ffb/quizsite/quizEngine/questions/Question.java
@@ -1,5 +1,61 @@
 package ffb.quizsite.quizEngine.questions;
 
-public class Question {
+import java.util.List;
 
+public class Question {
+  private String title;
+  private String text;
+  private List<String> options;
+  private int[] answers;
+  private int id;
+
+  public Question() {}
+
+  public Question(int id, String text, String title, List<String> options, int[] answers) {
+    this.id = id;
+    this.text = text;
+    this.title = title;
+    this.options = options;
+    this.answers = answers;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public List<String> getOptions() {
+    return options;
+  }
+
+  public void setOptions(List<String> options) {
+    this.options = options;
+  }
+
+  public int[] getAnswers() {
+    return answers == null ? new int[0] : answers;
+  }
+
+  public void setAnswers(int[] answers) {
+    this.answers = answers;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
 }
