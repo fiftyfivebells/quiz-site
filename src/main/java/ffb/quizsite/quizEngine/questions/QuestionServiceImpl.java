8a897793e9a5811a5496c3f7ae6b2b495242cb27
Stephen Bell <stephen.l.bell@gmail.com>
2021-03-30 21:08:19 -0400

    Implement methods for QuestionServiceImpl

--- src/main/java/ffb/quizsite/quizEngine/questions/QuestionServiceImpl.java
+++ src/main/java/ffb/quizsite/quizEngine/questions/QuestionServiceImpl.java
@@ -2,7 +2,9 @@ package ffb.quizsite.quizEngine.questions;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class QuestionServiceImpl implements QuestionService {
@@ -12,26 +14,34 @@ public class QuestionServiceImpl implements QuestionService {
 
   @Override
   public Question getQuestionById(Long id) {
-    return null;
+    return questionRepository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
   }
 
   @Override
   public List<Question> getAllQuestions() {
-    return null;
+    return (List<Question>) questionRepository.findAll();
   }
 
   @Override
   public Question createNewQuestion(Question question) {
-    return null;
+    return questionRepository.save(question);
   }
 
   @Override
   public Question updateQuestionById(Long id, Question question) {
-    return null;
+    Question toUpdate = questionRepository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    toUpdate.setAnswers(question.getAnswers());
+    toUpdate.setOptions(question.getOptions());
+    toUpdate.setTitle(question.getTitle());
+    toUpdate.setText(question.getText());
+
+    return questionRepository.save(toUpdate);
   }
 
   @Override
   public void deleteQuestionById(Long id) {
-
+    questionRepository.deleteById(id);
   }
 }
