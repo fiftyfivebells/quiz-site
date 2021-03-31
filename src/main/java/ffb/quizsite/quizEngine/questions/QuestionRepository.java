29b3db35f06cdcc0a9d22623c448ff7d6570f022
Stephen Bell <stephen.l.bell@gmail.com>
2021-03-30 20:54:37 -0400

    Create QuestionRepository interface

---
+++ src/main/java/ffb/quizsite/quizEngine/questions/QuestionRepository.java
@@ -0,0 +1,7 @@
+package ffb.quizsite.quizEngine.questions;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface QuestionRepository extends CrudRepository<Question, Long> {
+
+}
