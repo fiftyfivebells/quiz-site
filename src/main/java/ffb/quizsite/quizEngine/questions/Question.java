682379cf320c5e4fbfabde46b5c9d107389cc093
Stephen Bell <stephen.l.bell@gmail.com>
2021-04-01 13:29:53 -0400

    Add Entity annotation and create primary key on ID

--- src/main/java/ffb/quizsite/quizEngine/questions/Question.java
+++ src/main/java/ffb/quizsite/quizEngine/questions/Question.java
@@ -4,10 +4,15 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Question {
 
   @NotEmpty
@@ -21,6 +26,9 @@ public class Question {
   private List<String> options;
 
   private int[] answers;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private int id;
 
   public Question() {
